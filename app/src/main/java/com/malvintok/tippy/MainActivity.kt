13265a8e32a4efa0cd8d50cd8124e1367b55edca
Malvin Tok <2717867T@student.gla.ac.uk>
2022-12-04 10:06:02 +0800

    Initial commit

---
+++ app/src/main/java/com/malvintok/tippy/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.malvintok.tippy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.malvintok.tippy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
