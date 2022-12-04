8a323435a5f5e441576918538aa8ef6244e47c9c
Malvin Tok <2717867T@student.gla.ac.uk>
2022-12-04 20:30:34 +0800

    Add business logic

--- app/src/main/java/com/malvintok/tippy/MainActivity.kt
+++ app/src/main/java/com/malvintok/tippy/MainActivity.kt
@@ -2,7 +2,14 @@ package com.malvintok.tippy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.SeekBar
 import com.malvintok.tippy.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +18,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.seekBarTip.progress = INITIAL_TIP_PERCENT
+        binding.tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+        binding.seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $p1")
+                binding.tvTipPercentLabel.text = "$p1%"
+                computeTipAndTotal()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+
+        binding.etBaseAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+                computeTipAndTotal()
+            }
+
+        })
+    }
+
+    private fun computeTipAndTotal() {
+        if (binding.etBaseAmount.text.isEmpty()) {
+            binding.tvTipAmount.text = ""
+            binding.tvTotalAmount.text = ""
+            return
+        }
+        // 1. Get the value of the base and tip percent
+        val baseAmount = binding.etBaseAmount.text.toString().toDouble()
+        val tipPercent = binding.seekBarTip.progress
+
+        // 2. Compute the tip and total
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount = baseAmount + tipAmount
+
+        // 3. Update the UI
+        binding.tvTipAmount.text = "$" + "%.2f".format(tipAmount)
+        binding.tvTotalAmount.text = "$" + "%.2f".format(totalAmount)
     }
 }
